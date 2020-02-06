cdd2728e670db778ac8eeddd1044ab9bc1e5ec18
grammaticastre <mbpr@grammaticastres-macbook-pro.home>
2020-02-06 12:09:43 +0100

    Haha

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+
+const getForecast = (location) => {
+	let textLocation = document.getElementById('forecast-location')
+	let textForecast = document.getElementById('forecast-text')
+
+	textLocation.textContent = 'Loading...'
+	textForecast.textContent = ''
+	fetch(`http://localhost:3000/forecast?address=${location}`).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				return textLocation.textContent = data.error
+			}
+			textLocation.textContent = data.location
+			textForecast.textContent = data.forecast
+		}) 
+	})
+}
+
+const weatherForm = document.querySelector('button')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('click', (e) => {
+	e.preventDefault()
+	const location = search.value
+	getForecast(location)
+})
---
+++ src/app.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const app = express()
+
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setupd Handlebars and Views directory
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirPath))
+
+app.get('/forecast', (req, res) => {
+	if (!req.query.address) {
+		return res.send({
+			error: 'Please enter a location'
+		})
+	}
+
+	geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+		if (error) {
+			return res.send({ error })
+		}
+		forecast(latitude, longitude, (error, {degrees, rainProbability} = {}) => {
+			if (error) {
+				return res.send({ error })
+			}
+			res.send({
+				location,
+				forecast: `Weather: ${degrees} degrees out. There is a ${rainProbability}% chance of rain.`,
+				address: req.query.address
+			});
+		})
+	 })
+})
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Ronan Merien'
+	})
+})
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About Me:',
+		name: 'Ronan Merien'
+	})
+})
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Need Help?',
+		name: 'Ronan Merien',
+		paragraph: 'You can contact me at: merienronan@gmail.com for help'
+	})
+})
+
+app.get('/products', (req, res) => {
+	if (!req.query.search) {
+		return res.send({
+			error: 'you must provide a search term'
+		})
+	}
+	req.query.search
+	res.send({
+		products: []
+	})
+})
+
+app.get('/help/*', (req, res) => {
+	res.render('404', {
+		title: 'Error 404',
+		name: 'Ronan Merien',
+		error: 'Help article not found.'
+	})
+})
+
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: 'Error 404',
+		name: 'Ronan Merien',
+		error: 'Page not found.'
+	})
+})
+
+// app.com
+// app.com/help
+// app.com/about
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000.')
+})
